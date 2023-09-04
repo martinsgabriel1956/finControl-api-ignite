7dbc5ecc2e94ac0683d0371e2bc57e59ae0b32ec
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-04 14:49:59 -0300

    feat: Handling env with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Test Transaction',
-  //     amount: 1000,
-  //   })
-  //   .returning('*')
-
-  // return transaction
-
   const transactions = await knex('transactions')
     .where('amount', 1000)
     .select('*')
@@ -23,7 +14,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server running!')
