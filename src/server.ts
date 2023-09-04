503cea4c141c030b1daefba0886d99845cbd871c
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-04 13:56:19 -0300

    feat: Performing queries with knex

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,21 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Test Transaction',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
+
+  // return transaction
+
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
