54f695f6d3dcfa35975cc655120a72b59e195336
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-05 14:38:28 -0300

    feat: Transactions creation

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance } from 'fastify'
+import { z as zod } from 'zod'
+import crypto from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = zod.object({
+      title: zod.string(),
+      amount: zod.number(),
+      type: zod.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { amount, title, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
