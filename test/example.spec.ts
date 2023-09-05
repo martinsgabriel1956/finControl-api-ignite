8345851a0ea422969245c52084c0f4aec6393461
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-05 20:46:30 -0300

    feat: Testing transaction creation

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('the user is able to create a new transaction', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toBe(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('User is able to create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
