670458ca486a537d02eeee46b63a6b40c5dcaa1a
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-05 15:30:09 -0300

    feat: Transactions summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -26,6 +26,16 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   })
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = zod.object({
       title: zod.string(),
