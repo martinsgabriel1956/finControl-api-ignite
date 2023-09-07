c78580a106f951b4707da8dd44dba7692e48bf65
Gabriel Martins <martinsgabriel1956@gmail.com>
2023-09-06 22:50:49 -0300

    feat: Deploy app in Render

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,8 +11,9 @@ const envSchema = zod.object({
   NODE_ENV: zod
     .enum(['development', 'production', 'test'])
     .default('production'),
+  DATABASE_CLIENT: zod.enum(['sqlite', 'pg']),
   DATABASE_URL: zod.string(),
-  PORT: zod.number().default(3333),
+  PORT: zod.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
